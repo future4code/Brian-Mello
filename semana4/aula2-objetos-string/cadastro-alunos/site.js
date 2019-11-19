feac86ce30a1e995e6e222d60eab84dad634b36c
G B S <brianmmello@gmail.com>
2019-11-19 15:33:32 -0300

    foi adicionado uma validação de email e de número de telefone e também um alert caso o aluno seja cadastrado, ou os dados da validação estejam errados

--- semana4/aula2-objetos-string/cadastro-alunos/site.js
+++ semana4/aula2-objetos-string/cadastro-alunos/site.js
@@ -21,17 +21,39 @@ function cadastrar() {
     const genero = document.getElementById("generoAluno").value
     const dados = document.getElementById("listaDeCadastro")
     
-    dados.innerHTML += "<div id='cadastros'>" + "<p> Nome: " + nome + "</p>" + 
+    if (document.getElementById("emailAluno").value==="" || document.getElementById("emailAluno").value.indexOf('@')===-1 || document.getElementById("emailAluno").value.indexOf(".")===-1) {
+        alert("Email inválido");
+        document.getElementById("email").focus();
+        document.getElementById("nomeAluno").value = ""
+        document.getElementById("idadeAluno").value = ""
+        document.getElementById("telefoneAluno").value = ""
+        document.getElementById("emailAluno").value = ""
+        document.getElementById("generoAluno").value = ""
+        return false;
+    } else if (document.getElementById("telefoneAluno").value==="" || document.getElementById("telefoneAluno").value.indexOf('(')===-1 || document.getElementById("telefoneAluno").value.indexOf(')')===-1 || document.getElementById("telefoneAluno").value.indexOf("-")===-1) {
+            alert("Telefone inválido");
+            document.getElementById("telefoneAluno").focus();
+            document.getElementById("nomeAluno").value = ""
+            document.getElementById("idadeAluno").value = ""
+            document.getElementById("telefoneAluno").value = ""
+            document.getElementById("emailAluno").value = ""
+            document.getElementById("generoAluno").value = ""
+        return false;
+    }else {
+        dados.innerHTML += "<div id='cadastros'>" + "<p> Nome: " + nome + "</p>" + 
                         "<p> Idade: " + idade + "</p>" +
                         "<p> Telefone: " + telefone + "</p>" + 
                         "<p> Email: " + email + "</p>" + 
                         "<p> Gênero: " + genero + "</p>" + "</div>" 
 
-    document.getElementById("nomeAluno").value = ""
-    document.getElementById("idadeAluno").value = ""
-    document.getElementById("telefoneAluno").value = ""
-    document.getElementById("emailAluno").value = ""
-    document.getElementById("generoAluno").value = ""
+        alert("O Aluno " + nome + " foi cadastrado com sucesso.") 
+
+        document.getElementById("nomeAluno").value = ""
+        document.getElementById("idadeAluno").value = ""
+        document.getElementById("telefoneAluno").value = ""
+        document.getElementById("emailAluno").value = ""
+        document.getElementById("generoAluno").value = ""
+    }
 }
 
 
