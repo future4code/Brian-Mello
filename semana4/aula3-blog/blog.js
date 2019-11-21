fa75dbc832125d585180e91b20ef3e6e7a6796b2
G B S <brianmmello@gmail.com>
2019-11-21 14:33:40 -0300

    criado o html com o fomulário com os dados necessários para um  post

---
+++ semana4/aula3-blog/blog.js
@@ -0,0 +1,12 @@
+const arrayDeDados = []
+
+class Post {
+    constructor (titulo, autor, conteudo, data, descricao) {
+        this.titulo = titulo
+        this.autor = autor
+        this.autor = conteudo
+        this.autor = data
+        this.autor = descricao
+    }
+
+}
