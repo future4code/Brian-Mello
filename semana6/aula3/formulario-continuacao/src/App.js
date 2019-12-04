b6172ff37802526dffc8e6f21a36652e678c665e
Brian de Paula Mello <35143307+Brian-Mello@users.noreply.github.com>
2019-12-04 17:17:56 -0300

    Projeto To-do list incompleto

---
+++ semana6/aula3/formulario-continuacao/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import FormTodoList from './components/FormTodoList/FormTodoList';
+
+function App() {
+  return (
+    <div className="App">
+      <FormTodoList/>
+      
+    </div>
+  );
+}
+
+export default App;
