f1623b733982803a938da336bab5af014ce8ec32
Brian de Paula Mello <35143307+Brian-Mello@users.noreply.github.com>
2020-01-28 17:26:39 -0300

    testes em 43%

--- semana12/aula2/src/actions/index.test.js
+++ semana12/aula2/src/actions/index.test.js
@@ -1,24 +1,25 @@
-// import React from 'react'
-// import {shallow} from 'enzyme'
-// import renderer from "react-test-renderer";
-// import { setPostAction, } from "./index"
+import React from 'react'
+import {shallow} from 'enzyme'
+import renderer from "react-test-renderer";
+import { setPostAction, } from "./index"
 
-// describe('Teste Container do Reducer Posts', () => {
-//     it('Testando a ação de criar posts', () => {
-//         // Preparação
-//         const arrayDePosts = [{
-//             username: "Brian",
-//             votesCount: 1,
-//             userVoteDirection: 1,
-//             commentsNumber: 2,
-//             id: 'XFM8JtoESiWKqCml3Rjz',
-//             text: 'Oi gente! Bem vindos ao 4eddit :)'
-//         }]
+describe('Teste Container do Reducer Posts', () => {
+    it('Testando a ação de criar posts', () => {
+        // Preparação
+        const arrayDePosts = [{
+            username: "Brian",
+            votesCount: 1,
+            userVoteDirection: 1,
+            commentsNumber: 2,
+            id: 'XFM8JtoESiWKqCml3Rjz',
+            text: 'Oi gente! Bem vindos ao 4eddit :)'
+        }]
 
-//         // Execução
-//         const action = setPostAction(arrayDePosts)
+        // Execução
+        const action = setPostAction(arrayDePosts)
 
-//         // Verificação
-//         expect(action.type).toEqual('SET_POSTS_ACTION')
-//     })
-// })
+        // Verificação
+        expect(action.type).toEqual('SET_POSTS_ACTION')
+        expect(action.payload).toEqual({posts: arrayDePosts})
+    })
+})
